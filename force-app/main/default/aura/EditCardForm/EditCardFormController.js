4cdc5e3c7aec18fdd6f0b117bea3b9fb45a17946
Vladislav Kobrusev <vladislav.kobrusev@gmail.com>
2020-04-30 15:53:07 +0300

    fixed toasts

--- force-app/main/default/aura/EditCardForm/EditCardFormController.js
+++ force-app/main/default/aura/EditCardForm/EditCardFormController.js
@@ -7,18 +7,15 @@
         if (!nameField || !nameField.replace(/\s/g, "").length > 0) {
             component.set("v.msg", "Title field should be filled");
             component.set("v.stateType", "ERROR");
+            helper.doFireEvent(component, event);
         } else {
             component.set("v.msg", "Card changed successfully!");
             component.set("v.stateType", "SUCCESS");
             component.find("editRecordForm").submit();
         }
+    },
 
-        var cardCreatedEvent = $A.get("e.c:cardCreated");
-
-        cardCreatedEvent.setParams({
-            "stateType" : component.get("v.stateType"),
-            "msg" : component.get("v.msg")
-        });
-        cardCreatedEvent.fire();
+    handleSave: function(component, event, helper) {
+        helper.doFireEvent(component, event);
     }
 })
